2331eadfafcff267de4b77e671e1c69b63ea8867
KeeganAles <ales2@mail.nmc.edu>
2018-02-07 17:19:19 -0500

    Changed the name of the project to "The Sales Tracker App"

--- Demo_TheTravelingSalesperson.S2_Starter/Properties/AssemblyInfo.cs
+++ Demo_TheTravelingSalesperson.S2_Starter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Demo_TheTravelingSalesperson.S2_Starter")]
+[assembly: AssemblyTitle("TheSalesTrackerApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Demo_TheTravelingSalesperson.S2_Starter")]
+[assembly: AssemblyProduct("TheSalesTrackerApp")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
