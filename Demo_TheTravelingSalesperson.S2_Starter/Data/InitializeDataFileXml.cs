4df5f04c435f4f6f50401ae7dccfae53f2997f06
KeeganAles <ales2@mail.nmc.edu>
2018-02-07 15:56:31 -0500

    Have up to InitializeSalesperson() method done in InitializeDataFileXml - Sprint 3

---
+++ Demo_TheTravelingSalesperson.S2_Starter/Data/InitializeDataFileXml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_TheTravelingSalesperson
+{
+    class InitializeDataFileXml
+    {
+        private Salesperson InitializeSalesperson()
+        {
+            Salesperson salesperson = new Salesperson()
+            {
+                FirstName = "Bonzo",
+                LastName = "Regan",
+                AccountID = "banana103",
+                CurrentStock = new Product(Product.ProductType.Furry, 20, false),
+                CitiesVisited = new List<string>()
+                {
+                    "Detroit",
+                    "Grand Rapids",
+                    "Ann Arbor"
+                }
+            };
+
+            return salesperson;
+        }
+    }
+}
